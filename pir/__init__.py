28715a50a4a70c8addc39ccf142253962816f0a3
kk6 <hiro.ashiya@gmail.com>
2015-11-13 02:12:18 +0900

    Initial implement uninstall command

--- pir/__init__.py
+++ pir/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+
 import pip
 import click
 
@@ -32,7 +34,7 @@ def cli():
               type=click.File('a'),
               help="Append package names to the given requirement file.")
 def install(packages, requirement):
-    """Install and write requirements file.
+    """Install packages and write requirements file.
 
     To append package names were installed in requirements file,
     if the 'pip install' was successful.
@@ -56,6 +58,50 @@ def install(packages, requirement):
     msg = "Append the following packages in {requirement}: {packages}"
     click.echo(msg.format(requirement=requirement.name,
                           packages=", ".join(packages)))
+
+
+@cli.command()
+@click.argument('packages', nargs=-1)
+@click.option('--requirement', '-r', default='requirements.in',
+              type=click.File('r+'),
+              help="Remove package names from the given requirement file.")
+@click.confirmation_option(help="Are you sure you want to uninstall these packages?")
+def uninstall(packages, requirement):
+    """Uninstall packages and remove from requirements file.
+
+    To Remove uninstalled packages from requirements file,
+    if the 'pip uninstall' was successful.
+
+    :param packages: Uninstall packages.
+    :param requirement: Output destination of left package names.
+    """
+    msg = (
+        "You must give at least one requirement to uninstall"
+        "(see 'pir --help')"
+    )
+    if not packages:
+        click.echo(click.style(msg, fg='red'))
+        return
+
+    raised = pip.main(["uninstall"] + ['-y'] + [pkg for pkg in packages])
+    if raised:
+        return
+
+    output = []
+    pattern = re.compile('^[\w0-9\-.]+')
+    for line in requirement.readlines():
+        match_obj = pattern.match(line)
+        if match_obj:
+            matched_pkg = match_obj.group().lower()
+            if matched_pkg not in packages:
+                output.append(line)
+        else:
+            output.append(line)
+    content = '\n'.join(output)
+    if output[-1] != '\n':
+        output += '\n'
+    with open(requirement.name, 'w') as f:
+        f.write(content)
 
 
 if __name__ == '__main__':
