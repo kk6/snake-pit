2e868a9c126791a085f59c7d06c2a551fec22415
kk6 <hiro.ashiya@gmail.com>
2015-11-14 23:50:41 +0900

    :wrench: Fix problem that cannot remove dependencies.

--- pit/cli.py
+++ pit/cli.py
@@ -6,7 +6,12 @@ import click
 
 from . import echoes
 from .groups import AliasedGroup
-from .utils import classify_installed_or_not, re_edit_requirements
+from .utils import (
+    get_installed_package_set,
+    classify_installed_or_not,
+    re_edit_requirements,
+    get_dependencies,
+)
 
 
 @click.group(cls=AliasedGroup)
@@ -28,6 +33,7 @@ def install(packages, requirement):
 
     :param packages: Install packages.
     :param requirement: Output destination of package names.
+
     """
     if not packages:
         echoes.warn("You must give at least one requirement to install"
@@ -69,6 +75,7 @@ def uninstall(packages, requirement):
 
     :param packages: Uninstall packages.
     :param requirement: Output destination of left package names.
+
     """
     msg = (
         "You must give at least one requirement to uninstall"
@@ -83,11 +90,17 @@ def uninstall(packages, requirement):
         if pkg in uninstalled_packages:
             # Already installed
             continue
-        if pip.main(["uninstall"] + ['-y'] + [pkg]):
+        if pkg not in get_installed_package_set():
+            echoes.err("{} is not installed".format(pkg))
+            continue
+        if pip.main(["uninstall"] + ['-y'] + get_dependencies(pkg)):
             # Uninstall failed.
             continue
         else:
             uninstalled_packages.append(pkg)
+
+    if not uninstalled_packages:
+        return
 
     content = re_edit_requirements(requirement.readlines(), packages)
     with open(requirement.name, 'w') as f:
